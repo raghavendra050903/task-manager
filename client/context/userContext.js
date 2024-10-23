3c88cc2eab2c2069358b5668a95ed5df4dbb4f3a
Raghavendra Kalkutaki <96181986+raghavendra050903@users.noreply.github.com>
2024-10-23 22:29:06 +0530

    Update userContext.js

--- client/context/userContext.js
+++ client/context/userContext.js
@@ -9,7 +9,7 @@ const UserContext = React.createContext();
 axios.defaults.withCredentials = true;
 
 export const UserContextProvider = ({ children }) => {
-  const serverUrl = "http://localhost:8000";
+  const serverUrl = "https://task-manager-1-k420.onrender.com";
 
   const router = useRouter();
 
