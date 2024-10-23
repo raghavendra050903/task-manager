afb4730def050c5d7fd8c2a9ca5d2329b90dc4ac
Raghavendra Kalkutaki <96181986+raghavendra050903@users.noreply.github.com>
2024-10-23 23:41:59 +0530

    Update authMiddleware.js

--- backend/src/middleware/authMiddleware.js
+++ backend/src/middleware/authMiddleware.js
@@ -1,6 +1,14 @@
 import asyncHandler from "express-async-handler";
 import jwt from "jsonwebtoken";
 import User from "../models/auth/UserModel.js";
+const cors = require('cors');
+
+app.use(cors({
+  origin: 'https://task-manager-nine-beige.vercel.app', // Specify your frontend URL here
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
+
 
 export const protect = asyncHandler(async (req, res, next) => {
   try {
